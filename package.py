ea8f2bf5405de60136bae3f330d1e02a3f5747ea
Roy Nieterau <roy_nieterau@hotmail.com>
2024-10-07 22:58:39 +0200

    Define app host name

--- package.py
+++ package.py
@@ -1,9 +1,10 @@
 name = "nuke"
 title = "Nuke"
 version = "0.2.5-dev.1"
-
+app_host_name = "nuke"
 client_dir = "ayon_nuke"
 
+ayon_server_version = ">=1.1.2"
 ayon_required_addons = {
     "core": ">0.3.2",
 }
