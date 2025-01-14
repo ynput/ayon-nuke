659222b8517a56e60d348ee09d553435fbcd734c
robin@ynput.io <robin@ynput.io>
2025-01-14 14:23:32 +0100

    Contribution to fix Nuke staging directory.

--- client/ayon_nuke/plugins/publish/collect_nuke_instance_data.py
+++ client/ayon_nuke/plugins/publish/collect_nuke_instance_data.py
@@ -58,10 +58,14 @@ class CollectInstanceData(pyblish.api.InstancePlugin):
         staging_dir_persistent = instance.data["transientData"].get(
             "stagingDir_persistent", False
         )
+        staging_dir_is_custom = instance.data["transientData"].get(
+            "stagingDir_is_custom", False
+        )
         if staging_dir:
             instance.data.update({
                 "stagingDir": staging_dir,
-                "stagingDir_persistent": staging_dir_persistent
+                "stagingDir_persistent": staging_dir_persistent,
+                "stagingDir_is_custom": staging_dir_is_custom,
             })
 
         self.log.debug("Collected instance: {}".format(
