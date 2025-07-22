249e3db447a45a61e884b0db99c785f8481f08f5
Roy Nieterau <roy_nieterau@hotmail.com>
2025-07-23 00:53:02 +0200

    When incrementing the workfile during publish, use the new context-based workfile API so that the current author information is stored with the saved file.

--- client/ayon_nuke/plugins/publish/increment_script_version.py
+++ client/ayon_nuke/plugins/publish/increment_script_version.py
@@ -1,8 +1,8 @@
-import nuke
 import pyblish.api
 
-from ayon_core.pipeline import OptionalPyblishPluginMixin
 from ayon_core.lib import version_up
+from ayon_core.host import IWorkfileHost
+from ayon_core.pipeline import registered_host, OptionalPyblishPluginMixin
 
 
 class IncrementScriptVersion(pyblish.api.ContextPlugin,
@@ -27,6 +27,27 @@ class IncrementScriptVersion(pyblish.api.ContextPlugin,
         assert all(result["success"] for result in context.data["results"]), (
             "Publishing not successful so version is not increased.")
 
-        path = context.data["currentFile"]
-        nuke.scriptSaveAs(version_up(path))
-        self.log.info('Incrementing script version')
+        current_filepath = context.data["currentFile"]
+        new_filepath = version_up(current_filepath)
+
+        host: IWorkfileHost = registered_host()
+        if hasattr(host, "save_workfile_with_context"):
+            from ayon_core.host.interfaces import SaveWorkfileOptionalData
+            host.save_workfile_with_context(
+                filepath=new_filepath,
+                folder_entity=context.data["folderEntity"],
+                task_entity=context.data["taskEntity"],
+                description="Incremented by publishing.",
+                # Optimize the save by reducing needed queries for context
+                prepared_data=SaveWorkfileOptionalData(
+                    project_entity=context.data["projectEntity"],
+                    project_settings=context.data["project_settings"],
+                    anatomy=context.data["anatomy"],
+                )
+            )
+        else:
+            # Backwards compatibility before:
+            # https://github.com/ynput/ayon-core/pull/1275
+            host.save_workfile(new_filepath)
+
+        self.log.debug(f"Incremented script version to: {new_filepath}")
