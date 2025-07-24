9d09162da19241d25ea324f13da14cc64feb24cf
Jakub Trllo <43494761+iLLiCiTiT@users.noreply.github.com>
2025-07-24 16:57:02 +0200

    use 'save_next_version'

--- client/ayon_nuke/plugins/publish/increment_script_version.py
+++ client/ayon_nuke/plugins/publish/increment_script_version.py
@@ -1,3 +1,5 @@
+import os
+
 import pyblish.api
 
 from ayon_core.lib import version_up
@@ -28,16 +30,17 @@ class IncrementScriptVersion(pyblish.api.ContextPlugin,
             "Publishing not successful so version is not increased.")
 
         current_filepath = context.data["currentFile"]
-        new_filepath = version_up(current_filepath)
-
         host: IWorkfileHost = registered_host()
-        if hasattr(host, "save_workfile_with_context"):
+        try:
+            # Function 'save_next_version' was introduced in ayon-core 1.5.0
+            from ayon_core.pipeline.workfile import save_next_version
             from ayon_core.host.interfaces import SaveWorkfileOptionalData
-            host.save_workfile_with_context(
-                filepath=new_filepath,
-                folder_entity=context.data["folderEntity"],
-                task_entity=context.data["taskEntity"],
-                description="Incremented by publishing.",
+
+            current_filename = os.path.basename(current_filepath)
+            save_next_version(
+                description=(
+                    f"Incremented by publishing from {current_filename}"
+                ),
                 # Optimize the save by reducing needed queries for context
                 prepared_data=SaveWorkfileOptionalData(
                     project_entity=context.data["projectEntity"],
@@ -45,9 +48,14 @@ class IncrementScriptVersion(pyblish.api.ContextPlugin,
                     anatomy=context.data["anatomy"],
                 )
             )
-        else:
-            # Backwards compatibility before:
-            # https://github.com/ynput/ayon-core/pull/1275
+        except ImportError:
+            # Backwards compatibility before ayon-core 1.5.0
+            self.log.debug(
+                "Using legacy `version_up`. Update AYON core addon to "
+                "use newer `save_next_version` function."
+            )
+            new_filepath = version_up(current_filepath)
             host.save_workfile(new_filepath)
 
+        new_filepath = host.get_current_workfile()
         self.log.debug(f"Incremented script version to: {new_filepath}")
