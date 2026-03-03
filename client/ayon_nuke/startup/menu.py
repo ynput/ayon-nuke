7a06716aa1eaf5e461e1537cb5211b375e5bea51
robin@ynput.io <robin@ynput.io>
2026-03-03 13:04:14 +0100

    Fix typo.

--- client/ayon_nuke/startup/menu.py
+++ client/ayon_nuke/startup/menu.py
@@ -2,7 +2,8 @@ from ayon_core.pipeline import registered_host
 
 
 # This code gets only called from GUI mode.
-# Unlike the farm, we do exepct a valid registered host at this time.
+# Unlike the none-GUI mode (e.g. farm),
+# we do expect a valid host at this time.
 nuke_host = registered_host()
 if nuke_host is None:
     raise RuntimeError("Cannot find expected registered Nuke host.")
