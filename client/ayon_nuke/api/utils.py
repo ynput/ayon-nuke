410fde6a5e4253faed75b5f53dff41117425a925
Roy Nieterau <roy_nieterau@hotmail.com>
2026-04-08 21:51:07 +0200

    Apply suggestion from @BigRoy

--- client/ayon_nuke/api/utils.py
+++ client/ayon_nuke/api/utils.py
@@ -127,7 +127,7 @@ def _submit_render_on_farm(node) -> bool:
 
     # Ensure CreateInstance is enabled.
     for instance in create_context.instances:
-        instance.data["active"] = node.name() == instance.transient_data["node"].name()
+        instance.data["active"] = node == instance.transient_data["node"]
 
     context = pyblish.api.Context()
     context.data["create_context"] = create_context
