af8ba9c17bd9e6f5971211bea1a27c632cae63e2
Roy Nieterau <roy_nieterau@hotmail.com>
2026-04-08 23:39:16 +0200

    Allow setting frame range from version data if frame start or frame end is 0

--- client/ayon_nuke/plugins/load/load_clip.py
+++ client/ayon_nuke/plugins/load/load_clip.py
@@ -188,7 +188,7 @@ class LoadClip(plugin.NukeLoader):
                     version_entity,
                     repre_entity
                 )
-            if first and last:
+            if first is not None and last is not None:
                 self._set_range_to_node(
                     read_node, first, last, start_at_workfile, slate_frames
                 )
