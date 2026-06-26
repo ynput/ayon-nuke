b84e6a128766ab4a25ac24c83b6f4c70694aa004
Vincent Ullmann <Vincent.Ullmann@googlemail.com>
2026-06-26 17:36:30 +0100

    update LoadImage

--- client/ayon_nuke/plugins/load/load_image.py
+++ client/ayon_nuke/plugins/load/load_image.py
@@ -1,9 +1,6 @@
 import nuke
 
 import qargparse
-import ayon_api
-
-from ayon_core.pipeline import load
 
 from ayon_nuke.api.lib import (
     get_imageio_input_colorspace
@@ -17,13 +14,14 @@ from ayon_nuke.api import (
     update_container,
     viewer_update_and_undo_stop,
     colorspace_exists_on_node,
+    plugin,
 )
 from ayon_core.lib.transcoding import (
     IMAGE_EXTENSIONS
 )
 
 
-class LoadImage(load.LoaderPlugin):
+class LoadImage(plugin.NukeLoader):
     """Load still image into Nuke"""
 
     product_base_types = {
@@ -138,9 +136,7 @@ class LoadImage(load.LoaderPlugin):
             for k in ["source", "fps"]:
                 data_imprint[k] = version_attributes.get(k, str(None))
 
-            read_node["tile_color"].setValue(int("0x4ecd25ff", 16))
-
-            return containerise(
+            container =  containerise(
                 read_node,
                 name=name,
                 namespace=namespace,
@@ -148,6 +144,9 @@ class LoadImage(load.LoaderPlugin):
                 loader=self.__class__.__name__,
                 data=data_imprint,
             )
+            self.update_node_color(read_node)  # after containerise
+
+            return container
 
     def switch(self, container, context):
         self.update(container, context)
@@ -189,10 +188,6 @@ class LoadImage(load.LoaderPlugin):
             )
 
         # Get start frame from version data
-        last_version_entity = ayon_api.get_last_version_by_product_id(
-            project_name, version_entity["productId"], fields={"id"}
-        )
-
         last = first = int(frame_number)
 
         self.set_colorspace_to_node(
@@ -215,15 +210,10 @@ class LoadImage(load.LoaderPlugin):
             "fps": str(version_attributes.get("fps")),
         }
 
-        # change color of node
-        if version_entity["id"] == last_version_entity["id"]:
-            color_value = "0x4ecd25ff"
-        else:
-            color_value = "0xd84f20ff"
-        read_node["tile_color"].setValue(int(color_value, 16))
-
         # Update the imprinted representation
         update_container(read_node, updated_dict)
+        self.update_node_color(read_node)
+
         self.log.info("updated to version: {}".format(
             version_entity["version"]
         ))
