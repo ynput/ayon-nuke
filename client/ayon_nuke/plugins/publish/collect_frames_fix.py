9aa7935a8f561f70141319d212b2ca05e6ba7ada
Jakub Trllo <43494761+iLLiCiTiT@users.noreply.github.com>
2026-07-20 14:54:44 +0200

    better handling of backwards compatibility on the plugin

--- client/ayon_nuke/plugins/publish/collect_frames_fix.py
+++ client/ayon_nuke/plugins/publish/collect_frames_fix.py
@@ -15,6 +15,9 @@ if typing.TYPE_CHECKING:
     from ayon_core.lib.attribute_definitions import AbstractAttrDef
 
 
+NOT_SET = object()
+
+
 class CollectFramesFixDefNuke(
     pyblish.api.InstancePlugin,
     AYONPyblishPluginMixin
@@ -33,6 +36,8 @@ class CollectFramesFixDefNuke(
     settings_category = "nuke"
 
     rewrite_version_enable = False
+
+    core_plugin = NOT_SET
 
     def process(self, instance):
         attribute_values = self.get_attr_values_from_data(instance.data)
@@ -90,46 +95,8 @@ class CollectFramesFixDefNuke(
     def get_attr_defs_for_instance(
         cls, create_context: CreateContext, instance: CreatedInstance
     ) -> list[AbstractAttrDef]:
-        # --- Backwards compatibility for plugin name in core ---
-        # Remove in future. If there is 'CollectFramesFixDef' plugin in
-        #   context and returns definitions, do not show this plugin
-        #   that means old ayon-core is used.
-        # TODO bump required 'core' version to package.py when removing this
-        for plugin in create_context.plugins_with_defs:
-            if plugin.__class__.__name__ == "CollectFramesFixDef":
-                # Backwards compatibility for old plugin name, remove in
-                #   future
-                attrs = plugin.get_attr_defs_for_instance(
-                    create_context, instance
-                )
-                if attrs:
-                    return []
-
-        # If there is 'CollectFramesFixDef' plugin in context, copy its
-        #   values to this plugin values, so that user does not need to
-        #   re-enter them.
-        # - this auto-fix will work only once
-        # NOTE This backwards compatibility may stay here for a while, because
-        #   older workfiles can be used across multiple projects, some might
-        #   not be used for months.
-        if (
-            cls.__name__ not in instance.publish_attributes
-            and "CollectFramesFixDef" in instance.publish_attributes
-        ):
-            old_values = instance.publish_attributes["CollectFramesFixDef"]
-            frames_to_fix = old_values.get("frames_to_fix")
-            rewrite_version = old_values.get("rewrite_version")
-            new_values = {
-                key: value
-                for key, value in (
-                    ("frames_to_fix", frames_to_fix),
-                    ("rewrite_version", rewrite_version),
-                )
-                if value is not None
-            }
-            instance.publish_attributes[cls.__name__] = new_values
-
-        # --- End of backwards compatibility ---
+        if not cls.handle_backwards_compatibility(create_context, instance):
+            return []
 
         attributes: list[AbstractAttrDef] = [
             TextDef(
@@ -150,3 +117,96 @@ class CollectFramesFixDefNuke(
             )
 
         return attributes
+
+    @classmethod
+    def handle_backwards_compatibility(
+        cls,
+        create_context: CreateContext,
+        instance: CreatedInstance,
+    ) -> bool:
+        """Handle compatibility with core's 'CollectFramesFixDef' plugin.
+
+        There are 3 possible scenarios:
+        1. This plugin was already used on an instance and stored values into
+            instance.publish_attributes. In that case this plugin is used.
+        2. This plugin was not used, and core's plugin is available and
+            returns attribute definitions. In this case, this plugin should
+            not be used to keep backwards compatibility -> do not show the
+            same attributes twice.
+        3. This plugin was not used, and core's plugin is not available
+            or does not return attribute definitions. In this case,
+            this plugin should be used and old values from core's plugin
+            should be converted to this plugin's values.
+
+        Stept to remove backwards compatibility:
+        - [ ] Remove core plugin search and usage -> will require bump of core
+            addon compatibility in 'package.py'.
+        - [ ] Remove conversion of old values -> this is NOT related to addon
+            versions, but to workfiles. This might stay here for a long time.
+
+        Returns:
+            bool: True if this plugin should be used, False if core's plugin
+                should be used.
+
+        """
+        # If there is 'CollectFramesFixDef' plugin in context and returns
+        #   definitions, do not show this plugin that means old ayon-core
+        #   is used.
+        # In case this plugin already did store values then use it anyways.
+        # - that can happen if studio downgraded to old core.
+        # TODO bump required 'core' version to package.py when removing
+        #   search for core addon
+        core_plugin_name = "CollectFramesFixDef"
+        # Cache core plugin to avoid searching it on each instance.
+        core_plugin = cls.core_plugin
+        if core_plugin is NOT_SET:
+            core_plugin = None
+            for plugin in create_context.plugins_with_defs:
+                if plugin.__class__.__name__ == core_plugin_name:
+                    core_plugin = plugin
+                    break
+            cls.core_plugin = core_plugin
+
+        if core_plugin is None:
+            # Make sure old values are not stored in instance
+            instance.publish_attributes.pop(core_plugin_name, None)
+            return True
+
+        # This plugin was already used -> use this plugin values
+        # - we could monkey patch the core plugin to use this plugin values
+        #   but the order in which this method is called is not deterministic.
+        if cls.__name__ in instance.publish_attributes:
+            return True
+
+        # Core does not have the plugin anymore
+        if core_plugin is not None:
+            # Core has the plugin, if it returns attribute definitions,
+            #   do not show this plugin, because it means old core is used.
+            attrs = core_plugin.get_attr_defs_for_instance(
+                create_context, instance
+            )
+            if attrs:
+                return False
+
+        # Convert old values to new plugin values and store them into instance
+        old_values = {}
+        if core_plugin_name in instance.publish_attributes:
+            # Remove the values if core addon is not available
+            if core_plugin is None:
+                old_values = instance.publish_attributes.pop(
+                    core_plugin_name
+                )
+            else:
+                old_values = instance.publish_attributes[core_plugin_name]
+
+        new_values = {
+            key: value
+            for key, value in (
+                ("frames_to_fix", old_values.get("frames_to_fix")),
+                ("rewrite_version", old_values.get("rewrite_version")),
+            )
+            if value is not None
+        }
+        instance.publish_attributes[cls.__name__] = new_values
+
+        return True
