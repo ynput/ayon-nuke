64e43d9e7c2fd10131fb8746b37f259fb38ea8f2
Kayla Man <moonyuet2008@hotmail.com>
2026-08-31 18:22:00 +0800

    remove the settings

--- server/settings/templated_workfile_build.py
+++ server/settings/templated_workfile_build.py
@@ -21,17 +21,13 @@ class TemplatedWorkfileProfileModel(BaseSettingsModel):
     keep_placeholder: bool = SettingsField(
         False,
         title="Keep placeholders")
-    execute_on_new_file: bool = SettingsField(
-        False,
-        title="Apply to New Scene"
-    )
-    execute_on_app_launch: bool = SettingsField(
-        True,
-        title="Apply on Nuke launch"
-    )
     create_first_version: bool = SettingsField(
         True,
-        title="Save first workfile version"
+        title="Save first workfile version",
+        description=(
+            "Automatically create the first version "
+            "of the workfile based on the template upon saving."
+        )
     )
 
 
