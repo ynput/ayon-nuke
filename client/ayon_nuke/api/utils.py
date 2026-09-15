db329dfb1418a4550fce1a201d5e34453303ed10
Jakub Trllo <43494761+iLLiCiTiT@users.noreply.github.com>
2026-09-15 09:59:36 +0200

    favorite defs are defined using dataclass

--- client/ayon_nuke/api/utils.py
+++ client/ayon_nuke/api/utils.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 import os
 
 import nuke
@@ -22,20 +23,27 @@ def render_single_frame(group_node: nuke.Node) -> None:
     nuke.execute(write_node.fullName(), frame, frame)
 
 
-def set_context_favorites(favorites=None):
+@dataclass
+class FavoriteDef:
+    name: str
+    path: str
+
+
+def set_context_favorites(favorites: list[FavoriteDef]) -> None:
     """Adding favorite folders to nuke's browser
 
     Arguments:
-        favorites (dict): couples of {name:path}
+        favorites (list[FavoriteDef]): list of favorite definitions
     """
-    favorites = favorites or {}
+    favorites = favorites or []
     icon_path = resources.get_resource("icons", "folder-favorite.png")
-    for name, path in favorites.items():
+    for favorite in favorites:
         nuke.addFavoriteDir(
-            name,
-            path,
+            favorite.name,
+            favorite.path,
             nuke.IMAGE | nuke.SCRIPT | nuke.GEO,
-            icon=icon_path)
+            icon=icon_path
+        )
 
 
 def get_node_outputs(node):
