0378c9d5ac46111b6741406255d63aee1cb2dba5
Kayla Man <64118225+moonyuet@users.noreply.github.com>
2026-09-17 00:28:21 +0800

    Update lib.py
    Co-authored-by: Roy Nieterau <roy_nieterau@hotmail.com>

--- client/ayon_nuke/api/lib.py
+++ client/ayon_nuke/api/lib.py
@@ -848,7 +848,7 @@ def check_inventory_versions(show_popup=False):
                 log.warning("Scene has outdated content.")
                 _show_outdated_content_popup()
         except Exception as error:
-            log.warning(error)
+            log.warning(error, exc_info=True)
 
     # Colorize nodes
     try:
